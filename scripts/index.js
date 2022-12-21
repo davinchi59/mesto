5d8393dcf9185d16ed2d1117e84fd177ae0dc373
davinchi59 <mirnuy1296@mail.ru>
2022-12-21 11:49:34 +0700

    fix: Review 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,132 +1,111 @@
-const initialCards = [
-	{
-		name: 'Архыз',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-	},
-	{
-		name: 'Челябинская область',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-	},
-	{
-		name: 'Иваново',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-	},
-	{
-		name: 'Камчатка',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-	},
-	{
-		name: 'Холмогорский район',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-	},
-	{
-		name: 'Байкал',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-	},
-]
+const profileName = document.querySelector('.profile__name')
+const profileDescriprion = document.querySelector('.profile__description')
 
-const profileNameEl = document.querySelector('.profile__name')
-const profileDescrEl = document.querySelector('.profile__description')
-
-const profileEditBtnEl = document.querySelector('.profile__edit-btn')
-const addPostBtnEl = document.querySelector('.profile__add-btn')
+const profileEditButton = document.querySelector('.profile__edit-btn')
+const postAddButton = document.querySelector('.profile__add-btn')
 
 const popupEditProfile = document.querySelector('.popup_type_edit-profile')
 const popupAddPost = document.querySelector('.popup_type_add-post')
 const popupImage = document.querySelector('.popup_type_image')
 
-const editProfileFormEl = popupEditProfile.querySelector('.popup__form')
-const editProfileInputNameEl = editProfileFormEl.querySelector(
+const profileEditForm = popupEditProfile.querySelector('.popup__form')
+const profileEditInputName = profileEditForm.querySelector(
 	'.popup__input_type_name'
 )
-const editProfileInputDescriptionEl = editProfileFormEl.querySelector(
+const profileEditInputDescription = profileEditForm.querySelector(
 	'.popup__input_type_descr'
 )
 
-const addPostFormEl = popupAddPost.querySelector('.popup__form')
-const addPostInputNameEl = addPostFormEl.querySelector(
-	'.popup__input_type_place'
-)
-const addPostInputLinkEl = addPostFormEl.querySelector(
-	'.popup__input_type_link'
-)
+const postAddForm = popupAddPost.querySelector('.popup__form')
+const postAddInputName = postAddForm.querySelector('.popup__input_type_place')
+const postAddInputLink = postAddForm.querySelector('.popup__input_type_link')
 
-const popupImageImgEl = popupImage.querySelector('.popup__image')
-const popupImageTitleEl = popupImage.querySelector('.popup__image-title')
+const popupImageElement = popupImage.querySelector('.popup__image')
+const popupImageTitle = popupImage.querySelector('.popup__image-title')
 
-const popupCloseBtns = document.querySelectorAll('.popup__close-btn')
+const popupCloseButtons = document.querySelectorAll('.popup__close-btn')
 
-const postsEl = document.querySelector('.posts')
+const templatePost = document
+	.querySelector('#post')
+	.content.querySelector('.post')
 
-function togglePopup(popup) {
-	popup.classList.toggle('popup_opened')
+const posts = document.querySelector('.posts')
+
+function openPopup(popup) {
+	popup.classList.add('popup_opened')
+}
+function closePopup(popup) {
+	popup.classList.remove('popup_opened')
 }
 
-function saveProfile(evt) {
-	profileNameEl.textContent = editProfileInputNameEl.value
-	profileDescrEl.textContent = editProfileInputDescriptionEl.value
+function handleSubmitProfileEditForm(evt) {
+	profileName.textContent = profileEditInputName.value
+	profileDescriprion.textContent = profileEditInputDescription.value
 	evt.preventDefault()
-	togglePopup(popupEditProfile)
+	closePopup(popupEditProfile)
 }
 
-function addPost(evt) {
-	const name = addPostInputNameEl.value
-	const link = addPostInputLinkEl.value
-	postsEl.prepend(getCardMurkup(name, link))
+function handleSubmitAddPostForm(evt) {
+	const name = postAddInputName.value
+	const link = postAddInputLink.value
+	posts.prepend(getCardMurkup(name, link))
 	evt.preventDefault()
-	togglePopup(popupAddPost)
+	closePopup(popupAddPost)
 }
 
-function getCardMurkup(name, link) {
-	const templateEl = document.querySelector('#post').content
-	const cardEl = templateEl.querySelector('.post').cloneNode(true)
+function handleProfileEditButtonClick() {
+	profileEditInputName.value = profileName.textContent
+	profileEditInputDescription.value = profileDescriprion.textContent
+	openPopup(popupEditProfile)
+}
 
-	const imageEl = cardEl.querySelector('.post__image')
-	imageEl.src = link
-	imageEl.alt = name
-	imageEl.addEventListener('click', () => {
-		popupImageImgEl.src = link
-		popupImageImgEl.alt = name
-		popupImageTitleEl.textContent = name
-		togglePopup(popupImage)
+function handlePostAddButtonClick() {
+	postAddInputName.value = ''
+	postAddInputLink.value = ''
+	openPopup(popupAddPost)
+}
+
+function handlePopupCloseButtonClick(evt) {
+	closePopup(evt.target.closest('.popup'))
+}
+
+function getCardMurkup({ name, link }) {
+	const card = templatePost.cloneNode(true)
+
+	const image = card.querySelector('.post__image')
+	image.src = link
+	image.alt = name
+	image.addEventListener('click', () => {
+		popupImageElement.src = link
+		popupImageElement.alt = name
+		popupImageTitle.textContent = name
+		openPopup(popupImage)
 	})
 
-	const titleEl = cardEl.querySelector('.post__title')
-	titleEl.textContent = name
+	const title = card.querySelector('.post__title')
+	title.textContent = name
 
-	const likeBtnEl = cardEl.querySelector('.post__like')
-	likeBtnEl.addEventListener('click', (evt) =>
+	const likeButton = card.querySelector('.post__like')
+	likeButton.addEventListener('click', (evt) =>
 		evt.target.classList.toggle('post__like_active')
 	)
-	const removeBtnEl = cardEl.querySelector('.post__remove')
-	removeBtnEl.addEventListener('click', (evt) =>
+	const removeButton = card.querySelector('.post__remove')
+	removeButton.addEventListener('click', (evt) =>
 		evt.target.closest('.post').remove()
 	)
 
-	return cardEl
+	return card
 }
 
-profileEditBtnEl.addEventListener('click', () => {
-	editProfileInputNameEl.value = profileNameEl.textContent
-	editProfileInputDescriptionEl.value = profileDescrEl.textContent
-	togglePopup(popupEditProfile)
-})
+profileEditButton.addEventListener('click', handleProfileEditButtonClick)
 
-addPostBtnEl.addEventListener('click', () => {
-	addPostInputNameEl.value = ''
-	addPostInputLinkEl.value = ''
-	togglePopup(popupAddPost)
-})
+postAddButton.addEventListener('click', handlePostAddButtonClick)
 
-popupCloseBtns.forEach((el) =>
-	el.addEventListener('click', (evt) => {
-		togglePopup(evt.target.closest('.popup'))
-	})
+popupCloseButtons.forEach((el) =>
+	el.addEventListener('click', handlePopupCloseButtonClick)
 )
 
-editProfileFormEl.addEventListener('submit', saveProfile)
-addPostFormEl.addEventListener('submit', addPost)
+profileEditForm.addEventListener('submit', handleSubmitProfileEditForm)
+postAddForm.addEventListener('submit', handleSubmitAddPostForm)
 
-initialCards.forEach(({ name, link }) =>
-	postsEl.append(getCardMurkup(name, link))
-)
+initialCards.forEach((card) => posts.append(getCardMurkup(card)))
